d7a2eb53263de1ded030147b28cbd48cc92f3972
p4n1k0 <gabre.tarick@gmail.com>
2025-02-21 20:50:08 -0300

    Implementa models da aplicacao

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -4,5 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
 public class Hotel {
-
+    [Key]
+    public int HotelId { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public int CityId { get; set; }
+    public City? City { get; set; }
+    public ICollection<Room>? Rooms { get; set; }
 }
--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -6,4 +6,21 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured == false)
+        {
+            var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Hotel>().HasOne(hotel => hotel.City).WithMany(city => city.Hotels).HasForeignKey(hotel => hotel.CityId);
+        modelBuilder.Entity<Hotel>().HasMany(hotel => hotel.Rooms).WithOne(room => room.Hotel).HasForeignKey(room => room.HotelId);
+    }
 }
