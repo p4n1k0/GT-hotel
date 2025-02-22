460dbf9cfb355b478bf38bb25f0ed5488b9e28b7
p4n1k0 <gabre.tarick@gmail.com>
2025-02-21 21:08:16 -0300

    endpoint GET /city

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -14,7 +14,11 @@ namespace TrybeHotel.Repository
         // 2. Desenvolva o endpoint GET /city
         public IEnumerable<CityDto> GetCities()
         {
-            throw new NotImplementedException();
+            return _context.Cities.Select(city => new CityDto
+            {
+                CityId = city.CityId,
+                Name = city.Name,
+            }).ToList();
         }
 
         // 3. Desenvolva o endpoint POST /city
